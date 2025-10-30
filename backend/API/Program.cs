b6a86f055a49d86173bd821d39533e12fd3024bd
agustinleonardi <agustinleonardi5@gmail.com>
2025-10-30 09:15:49 -0300

    modified program.cs

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("https://play-hunt-backend.netlify.app")
+        policy.WithOrigins("https://play-hunt-front.netlify.app")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
